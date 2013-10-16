8f64a0035db0f71d5bf24c87d02363de674ca9d9
Gilbert <gf@tf.uni-kiel.de>
2013-10-16 12:43:31 +0200

    .

--- analyse/stepintegrate.py
+++ analyse/stepintegrate.py
@@ -5,19 +5,19 @@ ValuesOut = list()
 
 try:
     Len = Analyse["Len"]
-    NumSteps = len(ValuesIn[0])/Len
+    NumSteps = int(len(ValuesIn[0])/Len)
+    if NumSteps != len(ValuesIn[0])/Len:
+        Msg.Error(2, "Rounding, thats bad")
 except:
     NumSteps = 1
     Len = len(ValuesIn[0])
-
 if len(ValuesIn) == 1:
-    for s in NumSteps:
+    for s in range(NumSteps):
         ValuesOut.append(numpy.trapz(ValuesIn[0][s*Len:(s+1)*Len]))
     if Options.DebugAnalyse:
         print(Options.Indent, "Trapz integral of", AxisIn[0], "with equal stepsize assumed:", ValuesOut)
 elif len(ValuesIn) == 2:
-    for s in NumSteps:
-        ValuesOut.append(numpy.trapz(ValuesIn[0][s*Len:(s+1)*Len]))
+    for s in range(NumSteps):
         ValuesOut.append(numpy.trapz(ValuesIn[0][s*Len:(s+1)*Len], ValuesIn[1][s*Len:(s+1)*Len]))
     if Options.DebugAnalyse:
         print(Options.Indent, "Trapz integral of", AxisIn[0], "with steps", AxisIn[1], ":", ValuesOut)
