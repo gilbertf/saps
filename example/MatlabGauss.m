494c8b3350b83c5fcaf057b159dbd1379779a56e
Gilbert <gf@tf.uni-kiel.de>
2014-02-28 12:37:12 +0100

    .

--- example/MatlabGauss.m
+++
@@ -1,7 +0,0 @@
-function MatlabGauss(x, Expectation, Variance, FilenameResults)
-    Sigma = sqrt(Variance);
-    pdf = (1/(Sigma*sqrt(2*pi))) * exp(-(x-Expectation)^2/(2*Variance));
-    cdf = 0.5 * (1+erf((x-Expectation)/sqrt(2*Variance)));
-    Complete = 1;
-    itsave(FilenameResults, x, Expectation, Variance, pdf, cdf, Sigma, Complete);
-end
