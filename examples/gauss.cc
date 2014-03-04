a40e5e7ce68c2312f712a682e7a303ee7303f6f6
Gilbert <gf@tf.uni-kiel.de>
2014-03-04 11:01:16 +0100

    .

--- examples/gauss.cc
+++ examples/gauss.cc
@@ -22,13 +22,8 @@ X(double, Sigma)
 
 int main(int argc, char *argv[]) {
 	#include "ParseParameters.inc"
-	Complete = 0; //special variable defined in the include to communicate the processing progress
 	Sigma = sqrt(Variance);
-	Complete = 0.1;
-	#include "WriteInLoop.inc"
 	pdf = (1/(Sigma*sqrt(2*pi))) * exp(-pow(x-Expectation,2)/(2*Variance));
-	Complete = 0.5;
-	#include "WriteInLoop.inc"
 	cdf = 0.5 * (1+erf((x-Expectation)/sqrt(2*Variance)));
 	Complete = 1;
 	#include "WriteOutputVars.inc"
