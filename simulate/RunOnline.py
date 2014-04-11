bbe02b049ddca6093a98cc1afe21b5cde07e824a
Gilbert <gf@tf.uni-kiel.de>
2014-04-11 13:28:01 +0200

    Using DebugSimulate option in RunOnline module

--- simulate/RunOnline.py
+++ simulate/RunOnline.py
@@ -1,5 +1,11 @@
 import os
 
+try:
+    DebugSimulate = Options.Config["Saps"]["DebugSimulate"]
+except:
+    DebugSimulate = None
+
 for Cmd in ListCmd:
-    #print("Running: ", Cmd)
+    if DebugSimulate:
+        print("Simulate: ", Cmd)
     os.system(Cmd)
