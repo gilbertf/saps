2672a44f9dce9ed12baf4fb69d6c08dd118fabe3
Gilbert <gf@tf.uni-kiel.de>
2014-06-24 09:49:25 +0200

    .

--- analyse/StepSumIntegrate.py
+++ analyse/StepSumIntegrate.py
@@ -3,21 +3,21 @@ import scipy.integrate
 
 ValuesOut = list()
 
-try:
-    Len = Analyse["StepLen"]
-    NumSteps = int(len(ValuesIn[0])/Len)
-    if NumSteps != len(ValuesIn[0])/Len:
-        Msg.Error(2, "Rounding, thats bad")
-except:
-    NumSteps = 1
-    Len = len(ValuesIn[0])
+
+StepLen = Analyse["StepLen"]
+NumSteps = int(numpy.floor(len(ValuesIn[0])/StepLen))
+if NumSteps != len(ValuesIn[0])/StepLen:
+    Msg.Error(2, "Rounding " + str(len(ValuesIn[0])/StepLen) + " to " + str(NumSteps) + ", thats bad")
+
 if len(ValuesIn) == 1:
     for s in range(NumSteps):
-        ValuesOut.append(numpy.sum(ValuesIn[0][s*Len:(s+1)*Len])/Len)
+        ValuesOut.append(numpy.sum(ValuesIn[0][s*StepLen:(s+1)*StepLen])/StepLen)
     if Options.DebugAnalyse:
         print(Options.Indent, "Sum integral of", AxisIn[0], "with equal stepsize assumed:", ValuesOut)
 else:
     print("We expect one input variables")
 
 ValuesOut = [ValuesOut]
+#print("Out",ValuesOut)
+#print("In", ValuesIn)
 
