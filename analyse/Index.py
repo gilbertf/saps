d3cd386c57fda570ade67046bf1eb1874890f7ae
Gilbert J. M. Forkel <gf@tf.uni-kiel.de>
2017-01-30 12:52:33 +0100

    .

--- analyse/Index.py
+++ analyse/Index.py
@@ -2,4 +2,18 @@ Len = len(ValuesIn[0])
 for v in ValuesIn[1:]:
     if Len != len(v):
         Msg.Error(3, "Not all vectors are of equal length")
-ValuesOut = [[i for i in range(Len)]]
+
+if "Start" in Analyse:
+    if "Stop" in Analyse:
+        Start = float(Analyse["Start"])
+        Stop = float(Analyse["Stop"])
+
+        Step = (Stop - Start) / (Len - 1)
+        ValuesOut = [[ (i * Step) + Start for i in range(Len) ]]
+    else:
+        Msg.Error(3, "Stop is missing")
+else:
+    ValuesOut = [[i for i in range(Len)]]
+
+if "Stop" in Analyse and not "Start" in Analyse:
+    Msg.Error(3, "Start is missing")
